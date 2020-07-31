f9e05c56f5c0f7328594fb76cd592dd6b4f80903
Joshua Carter <joshuacarter0426@gmail.com>
2020-07-31 17:06:41 -0400

    My Website is LACKING

---
+++ script.js
@@ -0,0 +1,23 @@
+/*function toNext()
+{
+    window.open("about.html", "_self");
+}
+
+let nextButton = document.querySelector('.NextButton');
+nextButton.addEventListener('click', toNext);
+
+function toPrev()
+{
+    window.open("index.html", "_self");
+}
+
+let previousButton = document.querySelector('.PreviousButton');
+previousButton.addEventListener('click', toPrev);
+
+function toInfo()
+{
+    window.open("info.html", "_self");
+}
+
+let infoButton = document.querySelector('.InfoButton');
+infoButton.addEventListener('click', toInfo);*/
