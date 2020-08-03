609d2b26ae4bf7997f242053a46d10b697452cec
Joshua Carter <joshuacarter0426@gmail.com>
2020-08-03 14:31:57 -0400

    Please Wrok...

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-/*function toNext()
+let name = document.querySelector(".user_name");
+let phoneNumber = document.querySelector(".user_number");
+let email = document.querySelector(".user_email");
+let message = document.querySelector(".user_message");
+let nameContainer = document.querySelector('.name_container');
+let numberContainer = document.querySelector('.number_container');
+let emailContainer = document.querySelector('.email_container');
+let messageContainer = document.querySelector('.message_container');
+let submitButton = document.querySelector(".submit_button");
+
+function clickSubmit(event)
 {
-    window.open("about.html", "_self");
+    event.preventDefault();
+    if (name.value == "" || phoneNumber.value == "" || email.value == "" || message.value == "")
+    {
+        alert('You missed a field!!!');
+    }
+    else
+    {
+        formContainer.style.display = 'none';
+        nameContainer.appendChild(name);
+        numberContainer.appendChild(phoneNumber);
+        emailContainer.appendChild(email);
+        messageContainer.appendChild(message);
+    }
 }
 
-let nextButton = document.querySelector('.NextButton');
-nextButton.addEventListener('click', toNext);
-
-function toPrev()
-{
-    window.open("index.html", "_self");
-}
-
-let previousButton = document.querySelector('.PreviousButton');
-previousButton.addEventListener('click', toPrev);
-
-function toInfo()
-{
-    window.open("info.html", "_self");
-}
-
-let infoButton = document.querySelector('.InfoButton');
-infoButton.addEventListener('click', toInfo);*/
+submitButton.addEventListener('click', clickSubmit);
