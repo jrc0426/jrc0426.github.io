e5032fadfe7d8f7f3b3bce94c37c8fe5a62ee4b6
Joshua Carter <joshuacarter0426@gmail.com>
2020-08-03 17:25:16 -0400

    Half Done

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let name = document.querySelector(".user_name");
-let phoneNumber = document.querySelector(".user_number");
-let email = document.querySelector(".user_email");
-let message = document.querySelector(".user_message");
-let nameContainer = document.querySelector('.name_container');
-let numberContainer = document.querySelector('.number_container');
-let emailContainer = document.querySelector('.email_container');
-let messageContainer = document.querySelector('.message_container');
+let name = document.getElementsByClassName("user_name")[0];
+let phoneNumber = document.getElementsByClassName("user_number")[0];
+let email = document.getElementsByClassName("user_email")[0];
+let message = document.getElementsByClassName("user_message")[0];
+let nameContainer = document.getElementsByClassName('name_container')[0];
+let numberContainer = document.getElementsByClassName('number_container')[0];
+let emailContainer = document.getElementsByClassName('email_container')[0];
+let messageContainer = document.getElementsByClassName('message_container')[0];
 let submitButton = document.querySelector(".submit_button");
+let inputContainer = document.querySelector("input");
 
-function clickSubmit(event)
+function onClick(event)
 {
     event.preventDefault();
     if (name.value == "" || phoneNumber.value == "" || email.value == "" || message.value == "")
@@ -17,12 +18,15 @@ function clickSubmit(event)
     }
     else
     {
-        formContainer.style.display = 'none';
-        nameContainer.appendChild(name);
-        numberContainer.appendChild(phoneNumber);
-        emailContainer.appendChild(email);
-        messageContainer.appendChild(message);
+        nameContainer.append(name.value);
+        numberContainer.append(phoneNumber.value);
+        emailContainer.append(email.value);
+        messageContainer.append(message.value);
+        inputContainer.value = "";
     }
 }
 
-submitButton.addEventListener('click', clickSubmit);
+submitButton.addEventListener('click', onClick);
+
+/*Uncaught TypeError: nameContainer.appendChild is not a function
+    at HTMLButtonElement.clickSubmit (script.js:22)*/
